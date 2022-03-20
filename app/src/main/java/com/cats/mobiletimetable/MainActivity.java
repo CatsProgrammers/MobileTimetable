7970169c02e97185d254e916f1995506d55239fe
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-03-20 15:06:23 +0300

    Нормальный вызов календаря для выбора даты

--- app/src/main/java/com/cats/mobiletimetable/MainActivity.java
+++ app/src/main/java/com/cats/mobiletimetable/MainActivity.java
@@ -2,13 +2,53 @@ package com.cats.mobiletimetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Calendar myCalendar= Calendar.getInstance();
+    EditText dateSelectEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateSelectEditText=findViewById(R.id.dateEditText);
+
+        //Обработка активности с выбором даты
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+
+        //Нажатие на выбор даты
+        dateSelectEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
+
+    private void updateLabel(){
+        String myFormat="dd.MM.yyyy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.getDefault());
+        dateSelectEditText.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
 }
