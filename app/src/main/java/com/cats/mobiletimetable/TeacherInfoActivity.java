f633dc977e8642b26f98874b44afaaeafa8bcad3
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-03-20 13:03:03 +0300

    App init

---
+++ app/src/main/java/com/cats/mobiletimetable/TeacherInfoActivity.java
@@ -0,0 +1,14 @@
+package com.cats.mobiletimetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_info);
+    }
+}
